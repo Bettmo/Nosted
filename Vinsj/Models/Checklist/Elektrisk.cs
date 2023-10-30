69f3026e8dd61d0f94b42b5ef8a33bec0af50c61
HermanKE <herman.kjosterud.erlingsen@gmail.com>
2023-10-30 21:06:52 +0100

    Oppretttet mappen og filer for db sjekklisten

---
+++ Vinsj/Models/Checklist/Elektrisk.cs
@@ -0,0 +1,38 @@
+using Vinsj.Models.Dashboard;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Elektrisk
+{
+    public int ID { get; set; }
+    
+    [ForeignKey("ServiceOrdre")]
+    public int ServiceOrdreID { get; set; } // ForeignKey for ServiceOrdre
+    public int Ordrenummer { get; set; }
+    
+    public string? sjekkpunkt { get; set; } // Henter Sjekkpunktet
+    public bool ok { get; set; } // Bool som sjekker om ok er true
+    public bool borSkiftes { get; set; } // Bool som sjekker om borSKiftes er true
+    public bool defekt { get; set; } // Bool som sjekker om defekt er true
+    public string? Kommentar { get; set; } // Kommentarfelt
+    
+    public ServiceOrdre? ServiceOrdre { get; set; }
+}
+
+public class ElektriskListViewModel
+{
+    public List<Elektrisk> ElektriskListe { get; set; } = new List<Elektrisk>();
+    public ServiceOrdre? ServiceOrdreInfo { get; set; } 
+    
+    public int ServiceOrdreID { get; set; } 
+    public int Ordrenummer { get; set; } 
+    public string? Produkttype { get; set; } 
+    public string? Årsmodell { get; set; } 
+    public string? Serienummer { get; set; } 
+    public string? Status { get; set; } 
+    public string? Fornavn { get; set; } 
+    public string? Etternavn { get; set; } 
+    public string? Telefonnummer { get; set; } 
+    public string? Feilbeskrivelse { get; set; } 
+    public string? KommentarFraKunde { get; set; } 
+    public string? Kommentar { get; set; } 
+}
