e99534f81330d95f492b7ab45a61ed2f98a91813
Ericb <ericbbettmo@gmail.com>
2023-11-04 23:10:43 +0100

    more fixes #41

--- Vinsj/wwwroot/js/navigation.js
+++ Vinsj/wwwroot/js/navigation.js
@@ -9,12 +9,12 @@ const logoImage = document.getElementById("logoImage");
 
         if (sidebar.classList.contains("close")) {
             // Change the image source to another logo when sidebar is closed
-            logoImage.src = "mini_logo.jpg";
+            logoImage.src = "/mini_logo.jpg";
             logoImage.alt = "Mindre logo";
             logoImage.style.width = "60px"; // Adjust the width for the smaller logo
         } else {
             // Change the image source back to the original logo when sidebar is open
-            logoImage.src = "logo_nosted_endrett.jpg";
+            logoImage.src = "/logo_nosted_endrett.jpg";
             logoImage.alt = "logo";
             logoImage.style.width = "200px"; // Adjust the width for the original logo
         }
