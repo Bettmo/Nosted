8acd6c8c573c85c2266958709c1d7943da294ef0
Ericb <ericbbettmo@gmail.com>
2023-11-06 01:52:51 +0100

    ind css

--- Vinsj/Controllers/HomeController.cs
+++ Vinsj/Controllers/HomeController.cs
@@ -17,6 +17,11 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult Serviceskjema()
+    {
+        return View();
+    }
 
     public IActionResult Privacy()
     {
