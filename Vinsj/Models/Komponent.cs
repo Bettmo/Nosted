60f9638c863cfb080a855a2efbefcfcef26f7d12
Ericb <ericbbettmo@gmail.com>
2023-11-16 16:11:14 +0100

    Komponent Modell

---
+++ Vinsj/Models/Komponent.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vinsj.Models;
+
+public class Komponent_service
+{
+    [Key, Column(Order = 0)]
+    public int ServiceOrdreID_FK { get; set; }
+
+    [Key, Column(Order = 1)]
+    public string Komponent { get; set; }
+
+    public string Avdeling { get; set; }
+
+    public string Komponent_Status { get; set; }
+
+    // Navigation property for related ServiceInstance
+    [ForeignKey("ServiceOrdreID_FK")]
+    public ServiceInstance ServiceInstance { get; set; }
+}
