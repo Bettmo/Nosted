1d535244e849c5dfe314c2fccdbdea9a18e926c1
HermanKE <herman.kjosterud.erlingsen@gmail.com>
2023-11-21 22:19:35 +0100

    Utkast på sjekkliste

--- Vinsj/Controllers/HomeController.cs
+++ Vinsj/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Sjekkliste()
     {
         return View();
     }
