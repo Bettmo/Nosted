5f3ffe5a2b2d4574bbe137fc693df178352b3d5c
Trym123 <trymlundby@gmail.com>
2023-11-27 17:32:08 +0100

    eric

---
+++ Vinsj/Models/SignaturerModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Vinsj.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+    public class SignaturerModel
+    {
+    [Key]
+    [ForeignKey("ServiceOrdre")]
+    public int ServiceOrdreIDFK { get; set; }
+
+    public string? Kommentar { get; set; }
+    public string? Signatur { get; set; }
+    public string? Dato { get; set; }
+
+    // Navigasjon for fremmednøkkel
+    public ServiceOrdre ServiceOrdre { get; set; }
+}
+}
